38c72d9e9cff8ddb3277f2bf20138eab4d76adcb
Anthony Mills <am1037@gmail.com>
2026-05-13 17:52:30 -0400

    Added vitest and Testing Library

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import path from "path";
 
 import { defineConfig } from "vite";
@@ -11,5 +12,10 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    globals: true, // Allows using describe, expect, etc. without importing
+    environment: "jsdom", // Simulates a browser environment
+    setupFiles: "./src/test/setup.ts", // Runs before each test
   },
 });
