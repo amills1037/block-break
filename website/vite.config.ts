30a93502fccf5d412576a2dfce9fad899e5d51d2
Anthony Mills <am1037@gmail.com>
2026-05-14 15:13:02 -0400

    Added missing website files

---
+++ website/vite.config.ts
@@ -0,0 +1,21 @@
+/// <reference types="vitest/config" />
+import path from "path";
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  test: {
+    globals: true, // Allows using describe, expect, etc. without importing
+    environment: "jsdom", // Simulates a browser environment
+    setupFiles: "./src/test/setup.ts", // Runs before each test
+  },
+});
