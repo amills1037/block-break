b8a620f63384c10136b5322551b5757f7e9fcb3e
Anthony Mills <am1037@gmail.com>
2026-06-10 19:09:10 +0000

    Updated client style

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -1,9 +1,18 @@
 function Header() {
-  return (
-    <header>
-      <h1>Block Break - Client</h1>
-    </header>
-  );
+    return (
+        <header className="px-3 py-4 flex gap-2 bg-teal-700 ring ring-gray-900">
+            {/* "logo" from https://fonts.google.com/icons arrow back */}
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 -960 960 960"
+                fill="currentColor"
+                stroke="currentColor"
+                className="size-6 text-white"
+            >
+                <path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z" />
+            </svg>
+        </header>
+    );
 }
 
 export default Header;
--- client/src/components/layout/Main.tsx
+++ client/src/components/layout/Main.tsx
@@ -2,7 +2,7 @@ import WebASMApp from "@/components/WebASMApp";
 
 function Main() {
     return (
-        <main>
+        <main className="flex justify-center px-6 py-8 grow bg-white dark:bg-gray-800 shadow-md ring ring-gray-900/5">
             <WebASMApp />
         </main>
     );
