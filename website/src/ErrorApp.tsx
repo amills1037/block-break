fb090cc7ec7be5f01114b57e822b50a197d383ad
Anthony Mills <am1037@gmail.com>
2026-06-11 02:54:33 +0000

    Added error page to website (missing files)

---
+++ website/src/ErrorApp.tsx
@@ -0,0 +1,15 @@
+import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/Header";
+import Error from "@/components/layout/Error";
+
+function ErrorApp() {
+    return (
+        <>
+            <Header />
+            <Error />
+            <Footer />
+        </>
+    );
+}
+
+export default ErrorApp;
---
+++ website/src/components/layout/Error.tsx
@@ -0,0 +1,11 @@
+function Error() {
+    return (
+        <main className="flex justify-center px-6 py-8 grow bg-white dark:bg-gray-800 shadow-md ring ring-gray-900/5">
+            <h2 className="text-4xl text-gray-900 dark:text-white">
+                Error: 404 Not Found
+            </h2>
+        </main>
+    );
+}
+
+export default Error;
